c119a009884a88b43281c147fbd3d5521cf09c76
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-12-08 17:07:45 -0600

    Edited interface

--- Final_Project/src/gui/main_window.py
+++ Final_Project/src/gui/main_window.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("..")
 
 import tkinter as tk
+import pandas as pd
 from clustering.kmeans_clustering import KMeansClustering
 from clustering.spectral_clustering import SpectralClusteringModel
 import plotly.express as px
@@ -12,19 +13,52 @@ class MainWindow:
         self.root = root
         self.root.title("Movie Recommendation System")
 
-        self.kmeans_button = tk.Button(root, text="Aplicar K-medios", command=self.apply_kmeans)
-        self.kmeans_button.pack(pady=10)
+        # Obtener las dimensiones de la pantalla
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
 
-        self.spectral_button = tk.Button(root, text="Aplicar Agrupamiento Espectral", command=self.apply_spectral)
-        self.spectral_button.pack(pady=10)
+        # Calcular la posición para centrar la ventana
+        x_position = (screen_width - 600) // 2  # Ancho de la ventana principal
+        y_position = (screen_height - 400) // 2  # Altura de la ventana principal
 
-        # Agregar un componente para mostrar los resultados del clustering
+        # Establecer la posición de la ventana en el centro
+        self.root.geometry(f"600x400+{x_position}+{y_position}")
+
+        # Agregar un componente Listbox para mostrar la lista de películas
+        self.movie_listbox = tk.Listbox(root, selectmode=tk.SINGLE, height=10, width=40)
+        self.movie_listbox.grid(row=0, column=1, padx=10, pady=10, rowspan=2, sticky="nsew")
+
+        # Cargar la lista de películas al iniciar la interfaz
+        self.load_movie_list()
+
+        # Agregar botones, etiqueta de resultados y contenedor de gráficos usando grid
+        self.kmeans_button = tk.Button(root, text="Aplicar K-medios", command=self.apply_kmeans, width=12, height=2, bg="blue", fg="white", font=("Helvetica", 10))
+        self.kmeans_button.grid(row=0, column=0, pady=10, sticky="nsew") # Ubicar el botón en la fila 0 y columna 0 de la cuadrícula
+
+        # Crear botón para aplicar Agrupamiento Espectral
+        self.spectral_button = tk.Button(root, text="Aplicar Agrupamiento Espectral", command=self.apply_spectral, width=12, height=2, bg="green", fg="white", font=("Helvetica", 10))
+        self.spectral_button.grid(row=1, column=0, pady=10, sticky="nsew") # Ubicar el botón en la fila 1 y columna 0 de la cuadrícula
+
+        # Crear etiqueta para mostrar resultados
         self.result_label = tk.Label(root, text="")
-        self.result_label.pack(pady=10)
+        self.result_label.grid(row=2, column=0, padx=10, sticky="nsew") # Ubicar la etiqueta en la fila 2 y columna 0 de la cuadrícula con un relleno horizontal de 10 píxeles
 
-        # Agregar un componente para mostrar gráficos interactivos
+        # Crear un contenedor para gráficos
         self.graph_container = tk.Frame(root)
-        self.graph_container.pack(pady=10)
+        self.graph_container.grid(row=3, column=0, padx=10, sticky="nsew") # Ubicar el contenedor en la fila 3 y columna 0 de la cuadrícula con un relleno horizontal de 10 píxeles
+
+        # Configurar la expansión de las columnas y filas
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+        root.grid_columnconfigure(1, weight=1)
+
+    def load_movie_list(self):
+        # Cargar los datos del archivo movies.csv y agregarlos a la Listbox
+        movie_data = pd.read_csv('../../data/movies.csv')
+        for index, row in movie_data.iterrows():
+            movie_info = f"{row['Title']} ({row['Year']}), Genre: {row['Genre']}, Rating: {row['Rating']}"
+            self.movie_listbox.insert(tk.END, movie_info)
 
     def apply_kmeans(self):
         kmeans_model = KMeansClustering('../../data/movies.csv')
